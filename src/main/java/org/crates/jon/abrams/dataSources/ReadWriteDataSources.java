fce6777b4bf571207dd8c2cccc9928915cb65535
jon <zjon234@gmail.com>
2023-09-13 22:33:12 +0800

    first commit

--- src/main/java/org/crates/jon/abrams/dataSources/ReadWriteDataSources.java
+++ src/main/java/org/crates/jon/abrams/dataSources/ReadWriteDataSources.java
@@ -1,8 +1,22 @@
 package org.crates.jon.abrams.dataSources;
 
+import com.zaxxer.hikari.util.DriverDataSource;
+import org.apache.shardingsphere.driver.api.ShardingSphereDataSourceFactory;
+import org.apache.shardingsphere.infra.config.mode.ModeConfiguration;
+import org.apache.shardingsphere.infra.config.rule.RuleConfiguration;
+import org.apache.shardingsphere.readwritesplitting.api.ReadwriteSplittingRuleConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Properties;
 
 /**
  * @author jon
@@ -22,4 +36,24 @@ public class ReadWriteDataSources {
         this.dataSourcesProperties = dataSourcesProperties;
     }
 
+
+    @Bean
+    public DataSource dataSource() throws SQLException {
+        ModeConfiguration modeConfiguration = new ModeConfiguration("", null);
+        Collection<RuleConfiguration> configs = new LinkedList<>();
+        ReadwriteSplittingRuleConfiguration ruleConfiguration = new ReadwriteSplittingRuleConfiguration();
+        configs.add(ruleConfiguration);
+        return ShardingSphereDataSourceFactory.createDataSource(dataSourcesProperties.getDatabaseName(),
+                dataSourceMap(), configs, new Properties());
+    }
+
+
+    private Map<String, DataSource> dataSourceMap() {
+        Map<String, DataSource> dataSourceMap = new HashMap<>();
+        dataSourcesProperties.getShardingDataSources().forEach(ob -> {
+            DataSource dataSource = new DriverDataSource(ob.getUrl(), ob.getDriverClassName(), new Properties(), ob.getUsername(), ob.getPassword());
+            dataSourceMap.put(ob.getName(), dataSource);
+        });
+        return dataSourceMap;
+    }
 }
