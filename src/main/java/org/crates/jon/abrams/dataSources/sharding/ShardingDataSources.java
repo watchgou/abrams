b1a04529444a7812f50c8ca756801a8e9316ca3d
jon <zjon234@gmail.com>
2023-09-16 23:38:24 +0800

    first commit

--- src/main/java/org/crates/jon/abrams/dataSources/sharding/ShardingDataSources.java
+++ src/main/java/org/crates/jon/abrams/dataSources/sharding/ShardingDataSources.java
@@ -1,14 +1,30 @@
 package org.crates.jon.abrams.dataSources.sharding;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import org.apache.shardingsphere.driver.api.ShardingSphereDataSourceFactory;
+import org.apache.shardingsphere.infra.config.mode.ModeConfiguration;
+import org.apache.shardingsphere.infra.config.rule.RuleConfiguration;
+import org.apache.shardingsphere.mode.repository.standalone.StandalonePersistRepositoryConfiguration;
+import org.apache.shardingsphere.sharding.api.config.ShardingRuleConfiguration;
+import org.apache.shardingsphere.transaction.config.TransactionRuleConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Properties;
 
 /**
  * @author jon
  * @date 2023/9/16
  */
 
-//@Configuration
+@Configuration
 public class ShardingDataSources {
 
     private static final Logger log = LoggerFactory.getLogger(ShardingDataSources.class);
@@ -18,5 +34,40 @@ public class ShardingDataSources {
 
     public ShardingDataSources(ShardingProperties shardingProperties) {
         this.shardingProperties = shardingProperties;
+    }
+
+
+    //@Bean
+    public DataSource dataSource() throws SQLException {
+        return ShardingSphereDataSourceFactory.createDataSource(shardingProperties.getDatabaseName(), StandaloneMode(), dataSourceMap(), rule(), new Properties());
+    }
+
+
+    private Collection<RuleConfiguration> rule() {
+        Collection<RuleConfiguration> rule = new LinkedList<>();
+        ShardingRuleConfiguration shardingRuleConfiguration = new ShardingRuleConfiguration();
+        TransactionRuleConfiguration transactionRuleConfiguration = new TransactionRuleConfiguration("BASE", "Seata", new Properties());
+        rule.add(shardingRuleConfiguration);
+        rule.add(transactionRuleConfiguration);
+        return rule;
+    }
+
+
+    private ModeConfiguration StandaloneMode() {
+        StandalonePersistRepositoryConfiguration standalonePersistRepositoryConfiguration = new StandalonePersistRepositoryConfiguration("JDBC", new Properties());
+        return new ModeConfiguration("Standalone", standalonePersistRepositoryConfiguration);
+    }
+
+    private Map<String, DataSource> dataSourceMap() {
+        Map<String, DataSource> dataSourceMap = new HashMap<>();
+        shardingProperties.getShardingDataSources().forEach(ob -> {
+            DruidDataSource druidDataSource = new DruidDataSource();
+            druidDataSource.setUrl(ob.getUrl());
+            druidDataSource.setUsername(ob.getUsername());
+            druidDataSource.setPassword(ob.getPassword());
+            druidDataSource.setDriverClassName(ob.getDriverClassName());
+            dataSourceMap.put(ob.getName(), druidDataSource);
+        });
+        return dataSourceMap;
     }
 }
--- src/main/java/org/crates/jon/abrams/dataSources/sharding/ShardingProperties.java
+++ src/main/java/org/crates/jon/abrams/dataSources/sharding/ShardingProperties.java
@@ -1,13 +1,91 @@
 package org.crates.jon.abrams.dataSources.sharding;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
 /**
  * @author jon
  * @date 2023/9/16
  */
 
-//@Configuration
-//@ConfigurationProperties(prefix = "sharding.sphere")
+@Configuration
+@ConfigurationProperties(prefix = "sharding.sphere")
 public class ShardingProperties {
 
+
+    private String databaseName;
+
+    private List<ShardingDataSources> shardingDataSources;
+
+    public List<ShardingDataSources> getShardingDataSources() {
+        return shardingDataSources;
+    }
+
+    public void setShardingDataSources(List<ShardingDataSources> shardingDataSources) {
+        this.shardingDataSources = shardingDataSources;
+    }
+
+    public String getDatabaseName() {
+        return databaseName;
+    }
+
+    public void setDatabaseName(String databaseName) {
+        this.databaseName = databaseName;
+    }
+
+    public static class ShardingDataSources {
+        private String name;
+
+        private String driverClassName;
+
+        private String url;
+
+        private String username;
+
+        private String password;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getDriverClassName() {
+            return driverClassName;
+        }
+
+        public void setDriverClassName(String driverClassName) {
+            this.driverClassName = driverClassName;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+    }
 
 }
