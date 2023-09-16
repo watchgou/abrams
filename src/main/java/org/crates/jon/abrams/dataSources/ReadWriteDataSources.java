0073b0091fa7a274a2d86dfff3a36edfa3a032e4
jon <zjon234@gmail.com>
2023-09-16 15:55:58 +0800

    first commit

--- src/main/java/org/crates/jon/abrams/dataSources/ReadWriteDataSources.java
+++ src/main/java/org/crates/jon/abrams/dataSources/ReadWriteDataSources.java
@@ -2,9 +2,13 @@ package org.crates.jon.abrams.dataSources;
 
 import com.zaxxer.hikari.util.DriverDataSource;
 import org.apache.shardingsphere.driver.api.ShardingSphereDataSourceFactory;
+import org.apache.shardingsphere.infra.config.algorithm.AlgorithmConfiguration;
 import org.apache.shardingsphere.infra.config.mode.ModeConfiguration;
 import org.apache.shardingsphere.infra.config.rule.RuleConfiguration;
+import org.apache.shardingsphere.mode.repository.standalone.StandalonePersistRepositoryConfiguration;
 import org.apache.shardingsphere.readwritesplitting.api.ReadwriteSplittingRuleConfiguration;
+import org.apache.shardingsphere.readwritesplitting.api.rule.ReadwriteSplittingDataSourceRuleConfiguration;
+import org.apache.shardingsphere.readwritesplitting.api.transaction.TransactionalReadQueryStrategy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
@@ -39,12 +43,33 @@ public class ReadWriteDataSources {
 
     @Bean
     public DataSource dataSource() throws SQLException {
-        ModeConfiguration modeConfiguration = new ModeConfiguration("", null);
-        Collection<RuleConfiguration> configs = new LinkedList<>();
-        ReadwriteSplittingRuleConfiguration ruleConfiguration = new ReadwriteSplittingRuleConfiguration();
-        configs.add(ruleConfiguration);
-        return ShardingSphereDataSourceFactory.createDataSource(dataSourcesProperties.getDatabaseName(),
-                dataSourceMap(), configs, new Properties());
+
+
+        return ShardingSphereDataSourceFactory.createDataSource(dataSourcesProperties.getDatabaseName(), StandaloneMode(),
+                dataSourceMap(), rule(), new Properties());
+    }
+
+
+    private ModeConfiguration StandaloneMode() {
+        StandalonePersistRepositoryConfiguration standalonePersistRepositoryConfiguration = new StandalonePersistRepositoryConfiguration("JDBC", new Properties());
+        return new ModeConfiguration("Standalone", standalonePersistRepositoryConfiguration);
+    }
+
+
+    private Collection<RuleConfiguration> rule() {
+        Collection<RuleConfiguration> rule = new LinkedList<>();
+        ReadwriteSplittingRuleConfiguration shardingRuleConfiguration = new ReadwriteSplittingRuleConfiguration();
+        Collection<ReadwriteSplittingDataSourceRuleConfiguration> dataSources = new LinkedList<>();
+        ReadwriteSplittingDataSourceRuleConfiguration readwriteSplittingDataSourceRuleConfiguration =
+                new ReadwriteSplittingDataSourceRuleConfiguration(dataSourcesProperties.getDatabaseName(), dataSourcesProperties.getReadWriteProp().getWriteDataSourceName()
+                        , dataSourcesProperties.getReadWriteProp().getReadDataSourceNames(), TransactionalReadQueryStrategy.PRIMARY, "random");
+        shardingRuleConfiguration.setDataSources(dataSources);
+        Map<String, AlgorithmConfiguration> loadBalancers = new HashMap<>();
+        AlgorithmConfiguration algorithmConfiguration = new AlgorithmConfiguration("RANDOM", new Properties());
+        loadBalancers.put("random", algorithmConfiguration);
+        shardingRuleConfiguration.setLoadBalancers(loadBalancers);
+        rule.add(shardingRuleConfiguration);
+        return rule;
     }
 
 
