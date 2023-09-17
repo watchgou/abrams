f275f8b86926cdf604bf2a2f0f2724d5eed2675e
jon <zjon234@gmail.com>
2023-09-17 14:08:25 +0800

    first commit

--- src/test/java/org/crates/jon/abrams/ApplicationTest.java
+++ src/test/java/org/crates/jon/abrams/ApplicationTest.java
@@ -1,19 +1,22 @@
 package org.crates.jon.abrams;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockHttpServletRequestDsl;
-import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 
 @SpringBootTest
-@AutoConfigureMockMvc
 class ApplicationTest {
 
-    @Autowired
-    private MockMvc mockMvc;
 
+    @Test
+    public void test() {
+
+
+    }
 
 
 }
